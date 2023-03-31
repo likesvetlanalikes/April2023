198058105eba29ed615fbdf4daaa4303d26dde46
svetlana <svetlana_040285@mail.ru>
2023-03-31 20:37:28 +0300

    test commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import homeWork1.Seasons;
+
+public class Main {
+    public static void main(String[] args) {
+        Seasons x = new Seasons();
+        x.checkMonth();
+       x.test();
+
+
+
+    }
+}
---
+++ src/homeWork1/Seasons.java
@@ -0,0 +1,15 @@
+package homeWork1;
+
+public class Seasons {
+
+    public String t = "text";
+
+    public void checkMonth(){
+       int a = 1;
+    }
+
+
+    public void test(){
+
+    }
+}
