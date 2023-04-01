8682f51e0c4b37c8b2dba060643982db11bb93a8
svetlana <svetlana_040285@mail.ru>
2023-04-01 22:18:28 +0300

    add new HomeWork1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
-import homeWork1.Seasons;
+import homeWork1.Exit;
+import homeWork1.Sum;
 
 public class Main {
     public static void main(String[] args) {
-        Seasons x = new Seasons();
-        x.checkMonth();
-
-
+        Sum test4 = new Sum();
+        Sum.test4();
 
 
     }
 }
+
+
+//Seasons test3 = new Seasons();
+//        Seasons.test3();
+
+
+//letterEngl2 test2 = new letterEngl2();
+//        letterEngl2.test2();
+
+//   letterEngl1 test = new letterEngl1();
+//        letterEngl1.test();
+
+//   Exit exit = new Exit();
+// exit.consolInput();
--- src/homeWork1/Seasons.java
+++ src/homeWork1/Seasons.java
@@ -1,15 +1,40 @@
 package homeWork1;
 
+import java.util.Scanner;
 public class Seasons {
+    public static void test3() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите название месяца: ");
+        String monthName = scanner.nextLine().toLowerCase();
+        String season = "";
 
-    public String t = "text";
+        switch (monthName){
+            case "декабрь":
+            case "январь":
+            case "февраль":
+                season = "Зима";
+                break;
+            case "март":
+            case "апрель":
+            case "май":
+                season = "Весна";
+                break;
+            case "июнь":
+            case "июль":
+            case "август":
+                season = "Лето";
+                break;
+            case "сентябрь":
+            case "октябрь":
+            case "ноябрь":
+                season = "Осень";
+                break;
+            default:
 
-    public void checkMonth(){
-       int a = 1;
-    }
-
-
-    public void test(){
-
+                System.out.println("Ошибка: некорректное название месяца");
+                System.exit(1);
+                // завершение работы с кодом ошибки;
+        }
+        System.out.println(monthName + "относится к сезону" + season);
     }
 }
