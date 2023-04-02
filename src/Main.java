bfb7e4ab4e33ffb243d4ad81fd267f2559865861
svetlana <svetlana_040285@mail.ru>
2023-04-02 14:52:45 +0300

    add new HomeWork1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,34 @@
 import homeWork1.Exit;
-import homeWork1.Sum;
+import homeWork2.Fibonachi;
 
 public class Main {
     public static void main(String[] args) {
-        Sum test4 = new Sum();
-        Sum.test4();
+        Fibonachi Fib = new Fibonachi();
+        Fibonachi.Fib();
 
 
     }
 }
 
 
+
+//homeWork1
+//import homeWork1.Sum;
+//public class Main {
+//    public static void main(String[] args) {
+//        Sum test4 = new Sum();
+//        Sum.test4();
+
+//***
 //Seasons test3 = new Seasons();
 //        Seasons.test3();
 
-
+//****
 //letterEngl2 test2 = new letterEngl2();
 //        letterEngl2.test2();
-
+//******
 //   letterEngl1 test = new letterEngl1();
 //        letterEngl1.test();
-
+//******
 //   Exit exit = new Exit();
 // exit.consolInput();
