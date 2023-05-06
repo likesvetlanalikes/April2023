da0cadf331136beb28283b785b2598f997f7e4e8
Svetlana <121423073+likesvetlanalikes@users.noreply.github.com>
2023-05-06 14:57:08 +0300

    Add files via upload

---
+++ src/homeWork3/Deprecated.java
@@ -0,0 +1,20 @@
+package homeWork3;
+
+public class Deprecated {
+    package homeWork3;
+
+    public class MyDeprecated {
+        @Deprecated
+
+
+        public void perimeter() {
+            int side1 = 3;
+            int side2 = 5;
+            int side3 = 7;
+            int perimeterTriangle = side1 + side2 + side3;
+            System.out.println(perimeterTriangle);
+        }
+
+    }
+// Компилятор выдает предупреждение о нежелательном использовании элемента
+}
